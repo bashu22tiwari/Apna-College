496ef64697321ed4bc47c2affb021fe05b02e80b
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-01-11 18:38:07 +0530

    I am Finding the size of datatypes.

---
+++ datatypes/datatypes.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << sizeof(a) <<"\n" ;
+
+    char d;
+    cout << sizeof(d) <<"\n"  ;
+
+    float e;
+    cout << sizeof(e) <<"\n" ;
+
+    double v;
+    cout << sizeof(v) <<"\n";
+}

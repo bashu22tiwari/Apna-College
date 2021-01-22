883e43bad9eacbb15b294ad03817b6586b23f18d
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-01-22 20:27:44 +0530

    Tried some more conversions.

--- Function 3.0/decimaltobinary.cpp
+++ Function 3.0/decimaltobinary.cpp
@@ -3,17 +3,35 @@
 #include<iostream>
 using namespace std ;
 
+ int pow(int a,int b)
+ {
+     int ans=1;
+     if (b == 0)
+     {
+         return ans;
+     }
+     else if (b > 0){
+     for(int j=1; j<=b ; j++)
+     {
+         ans*=a;
+     }
+     return ans;
+     }
+ }
+
 int main()
 {
-    int n,rem,ans=0;
+    int n,rem,i,ans=0;
     cout << "Enter Decimal Number\n" ;
     cin >> n;
+    i=0;
 
-    while (n>=2)
+    while (n!=0)
     {
         rem = n%2;
-        ans += remainder*10 
+        ans += rem*pow(10,i);
         n=n/2;
+        i++;
     }
-    
+    cout << ans ;
 }
--- Function 3.0/decimaltooctal.cpp
+++ Function 3.0/decimaltooctal.cpp
@@ -1,20 +1,37 @@
-// In this i am going to make a program where we will be converting decimal number into octal number
+// In this i am going to make a program where we will be converting decimal number into binary number
 
 #include<iostream>
 using namespace std ;
 
+ int pow(int a,int b)
+ {
+     int ans=1;
+     if (b == 0)
+     {
+         return ans;
+     }
+     else if (b > 0){
+     for(int j=1; j<=b ; j++)
+     {
+         ans*=a;
+     }
+     return ans;
+     }
+ }
+
 int main()
 {
-    int n,rem;
+    int n,rem,i,ans=0;
     cout << "Enter Decimal Number\n" ;
     cin >> n;
+    i=0;
 
-cout << "1";
-    while (n>=2)
+    while (n!=0)
     {
         rem = n%8;
-        cout << rem ;
-        n=n/2;
+        ans += rem*pow(10,i);
+        n=n/8;
+        i++;
     }
-    
+    cout << ans ;
 }

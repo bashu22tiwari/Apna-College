5c7edffc7e472e901cf47cd27fa495a80d6d5af5
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-01-31 18:17:38 +0530

    Done basics on Character array.

---
+++ Character Array/basic.cpp
@@ -0,0 +1,13 @@
+// In this we will be going to learn about the character array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char array[100];
+    cin >> array ;
+
+    cout << array[2] ;
+    
+}

ab9aa5ed6e447e13e150c7e09eb3eb97c6bfe0c2
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-02-07 23:55:44 +0530

    I have started learning strings.

---
+++ Strings/basic.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str1="fam";
+    string str2="ily";
+    string str3;
+
+    
+
+    str1 = str1+str2;
+
+
+    
+
+    cout << str1;
+    return 0;
+}

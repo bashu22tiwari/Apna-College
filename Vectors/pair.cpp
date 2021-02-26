9dbe7a73d6562d6bfc367536b34229b64ded8143
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-02-26 23:08:12 +0530

    Vector and Pair.

---
+++ Vectors/pair.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    pair <int , int> p ;
+    p.first = 3 ;
+    p.second = 'f';
+
+    cout << p.first ;
+}

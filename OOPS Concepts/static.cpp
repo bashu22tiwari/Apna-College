b5d1f985ae7466b31776bd329225e40da1e5aed3
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-03-13 20:47:23 +0530

    OOPS Concepts.

---
+++ OOPS Concepts/static.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+class Student{
+
+public:
+
+int rollNumber;
+int age;
+
+static int totalStudents;
+
+Student()
+{
+    totalStudents += 1;
+}
+};
+
+int Student :: totalStudents = 0 ;
+
+int main()
+{
+    Student s1;
+    Student s2;
+    Student s3;
+    Student s4;
+
+    Student s5;
+    Student s6;
+
+    Student s7;
+    Student s8;
+
+    cout << Student :: totalStudents << endl;
+    
+    
+}

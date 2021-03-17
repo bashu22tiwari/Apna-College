ff49d42e6c23baab4102851ebcd1336c0c01eb48
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-03-18 01:48:31 +0530

    Geeks for Geeks

---
+++ Basics and Data types/datatypes/gcd.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <boost/math/common_factor.hpp> 
+using namespace std;
+
+int main()
+{
+    cout << __gcd(100,150) ;
+    cout << boost::math::lcm(100,150) ;
+}

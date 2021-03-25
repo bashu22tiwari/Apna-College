442b2152462ffd0704148a38fa0be21fd2fe962c
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-03-26 02:04:30 +0530

    Linked List

---
+++ Data Structures Coding Ninjas/Linked List/bestRecursive.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+#include <cstdlib>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+
+class Node{
+public:
+int data;
+Node *next;
+
+Node(int data)
+{
+    this->data=data;
+    next = NULL;
+}
+};
+
+
+Node *takeInput()
+{
+    int data;
+    cin >> data ;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while(data != -1)
+    {
+        Node *newNode = new Node(data);
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail->next = newNode;
+            tail = tail->next;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+Node *reversing(Node *head)
+{
+    if(head==NULL || head->next==NULL)
+    {
+        return head;
+    }
+
+    Node *smallAnswer = reversing(head->next);
+    
+    Node *tail = head->next;
+    tail->next = head;
+    head->next = NULL;
+    
+    return smallAnswer;
+
+    
+}
+
+
+void print(Node *head)
+{
+    Node *temp = head;
+
+    while(temp != NULL)
+    {
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+
+}
+
+int main()
+{
+    Node *head = takeInput();
+    
+    head = reversing(head);
+
+    print(head);
+}

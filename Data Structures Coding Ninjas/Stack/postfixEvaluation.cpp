f628eef925275d55ed5191cc044f0fab2ddc603e
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-04-24 22:51:41 +0530

    Queues On GFG

--- Data Structures Coding Ninjas/Stack/postfixEvaluation.cpp
+++ Data Structures Coding Ninjas/Stack/postfixEvaluation.cpp
@@ -7,5 +7,67 @@
 #include <queue>
 using namespace std;
 {
-    
+   class Solution{
+    public:
+    // The main function that returns value of a given postfix expression
+    int evaluatePostfix(string S)
+    {
+        stack<int> s;
+        
+        for(int i=0 ; i<S.size() ; i++){
+            
+            if(S[i]=='+'){
+                
+                int op1 = s.top();
+                s.pop();
+                int op2 = s.top();
+                s.pop();
+                
+                int op3 = op2+op1;
+                s.push(op3);
+                
+            }
+            
+            else if(S[i]=='-'){
+                
+                int op1 = s.top();
+                s.pop();
+                int op2 = s.top();
+                s.pop();
+                
+                int op3 = op2-op1;
+                s.push(op3);
+            }
+            
+            else if(S[i]=='*'){
+                
+                int op1 = s.top();
+                s.pop();
+                int op2 = s.top();
+                s.pop();
+                
+                int op3 = op2*op1;
+                s.push(op3);
+            }
+            else if(S[i]=='/'){
+                int op1 = s.top();
+                s.pop();
+                int op2 = s.top();
+                s.pop();
+                
+                int op3 = op2/op1;
+                s.push(op3);
+            }
+            else{
+                
+                
+                s.push(S[i]-'0');
+                
+            }
+            
+        }
+        
+        return s.top();
+    }
+};
 }

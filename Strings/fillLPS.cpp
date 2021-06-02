92fcb8fe7f61ae03753ab203f811caf060f483c1
Bashu Tiwari <55925918+bashu22tiwari@users.noreply.github.com>
2021-06-02 22:45:36 +0530

    Strings.

--- Strings/fillLPS.cpp
+++ Strings/fillLPS.cpp
@@ -22,3 +22,26 @@ void fillLPS(string str, int LPS[]){
         LPS[i] = longProperPrefixSuffix(str,i+1);
     }
 }
+
+voidfillLPSEfficiently(string str, int LPS[]){
+    int n = str.size();
+    int len = 0;
+    LPS[0] = 0;
+    int i=0;
+    (i<n){
+        if(str[i]==str[len]){
+            len++;
+            LPS[i] = len;
+            i++;
+        }
+        else{
+            if(len==0){
+                LPS[i] = 0;
+                i++;
+            }
+            else{
+                len = LPS[len-1];
+            }
+        }
+    }
+}
